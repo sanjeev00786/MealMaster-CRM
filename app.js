4b30a6856188703f9afed9447e3a333ca32af7ea
Gurmeet Singh <gsingh391@mylangara.ca>
2024-02-08 17:03:25 -0800

    feat: meal_plans APIs
    This commit introduces the implementation of the meal-plans APIs. Jira Ticket #57

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const driverRoutes = require('./server/routes/Driver/driver_routes.js')
 const customerRoutes = require('./server/routes/customer/customer_routes.js')
+const meal_plan_Routes = require('./server/routes/meal_plans/meal_plans_route.js')
 
 const port = 3000;
 
@@ -12,6 +13,9 @@ app.use('/api/drivers', driverRoutes);
 
 // Use the customer routes acting as a middleware
 app.use('/api/customer', customerRoutes);
+
+// Use the meal_plans routes acting as a middleware
+app.use('/api/provider/meal_plans', meal_plan_Routes);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
