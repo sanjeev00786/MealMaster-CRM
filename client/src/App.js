c7a2eb818866b3c95c0ea4316701e775db9a6475
rohxoxo <rohitnicbc@gmail.com>
2024-02-17 23:05:02 -0800

    feat: routes for the application
    Created a demo routes for the application to be used by each and every individual.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
+// import CustomerTable from './components/Dashboard';
 import supabase from './supabase';
+
+const PrivateRoute = ({ component: Component, session, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      session ? <Component {...props} /> : <Redirect to="/login" />
+    }
+  />
+);
 
 const App = () => {
   const [session, setSession] = useState(null);
-
   useEffect(() => {
     const fetchSession = async () => {
       try {
@@ -15,15 +25,11 @@ const App = () => {
         console.error('Error fetching session:', error.message);
       }
     };
-
     const authStateChangeHandler = (_event, session) => {
       setSession(session);
     };
-
     fetchSession();
-
     const { data: { subscription } } = supabase.auth.onAuthStateChange(authStateChangeHandler);
-
     return () => subscription.unsubscribe();
   }, []);
 
@@ -35,23 +41,23 @@ const App = () => {
       console.error('Logout failed:', error.message);
     }
   };
-
   return (
-    <div className="App">
-      {session ? (
-        <div>
-          <p>Welcome, {session.user.email}!</p>
-          {/* Add your authenticated content here */}
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      ) : (
-        <div>
-          <Login />
-        </div>
-      )}
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login" component={Login} />
+          {/* <Route path="/driver-login" component={Login} /> */}
+          <Route path="/signup" component={SignUp} />
+          {/* <PrivateRoute path="/dashboard" component={CustomerTable} session={session} />
+          <PrivateRoute path="/customers" component={Customers} session={session} />
+          <PrivateRoute path="/track-deliveries" component={TrackDeliveries} session={session} />
+          <PrivateRoute path="/delivery-schedule" component={DeliverySchedule} session={session} />
+          <PrivateRoute path="/social-media" component={SocialMedia} session={session} />
+          <PrivateRoute path="/drivers-dashboard" component={Drivers} session={session} />
+          <PrivateRoute path="/drivers-past-deliveries" component={Drivers} session={session} /> */}
+          <Redirect from="/" to={session ? '/dashboard' : '/login'} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
-
-export default App;
-
