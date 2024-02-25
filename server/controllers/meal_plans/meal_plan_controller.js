0b217b003585f7b9148f6a16eaebaf443e2880e0
Gurmeet Singh <gsingh391@mylangara.ca>
2024-02-25 07:03:53 -0800

    update model

--- server/controllers/meal_plans/meal_plan_controller.js
+++ server/controllers/meal_plans/meal_plan_controller.js
@@ -3,7 +3,7 @@ const mealPlanModel = require("../../models/meal_plans/meal_plans_models");
 exports.addMealPlan = async (req, res) => {
   try {
     const { provider_id, plan_name, price, description } = req.body;
-
+    console.log('controller', req.body);
     // console.log("Received data for meal plan:", req.body);
 
     const result = await mealPlanModel.createMealPlan({
@@ -29,13 +29,11 @@ exports.addMealPlan = async (req, res) => {
     }
   } catch (error) {
     // console.error(error);
-    res
-      .status(500)
-      .json({
-        status: 500,
-        success: false,
-        error: "Internal Server Error while creating meal plan",
-      });
+    res.status(500).json({
+      status: 500,
+      success: false,
+      error: "Internal Server Error while creating meal plan",
+    });
   }
 };
 
@@ -44,37 +42,31 @@ exports.getMealPlan = async (req, res) => {
     const { provider_id } = req.query;
 
     const result = await mealPlanModel.readMealPlan({
-      provider_id
+      provider_id,
     });
 
     if (result && result.success) {
-      res
-        .status(201)
-        .json({
-          status: 201,
-          success: true,
-          data: result.data,
-          message: result.message,
-        });
+      res.status(201).json({
+        status: 201,
+        success: true,
+        data: result.data,
+        message: result.message,
+      });
     } else {
-      res
-        .status(400)
-        .json({
-          status: 400,
-          success: false,
-          error:
-            "No meal plans found for the given provider_id,Failed to fetch meal plans",
-        });
+      res.status(400).json({
+        status: 400,
+        success: false,
+        error:
+          "No meal plans found for the given provider_id,Failed to fetch meal plans",
+      });
     }
   } catch (error) {
     // console.error(error);
-    res
-      .status(500)
-      .json({
-        status: 500,
-        success: false,
-        error: "Internal Server Error while fetching meal plan",
-      });
+    res.status(500).json({
+      status: 500,
+      success: false,
+      error: "Internal Server Error while fetching meal plan",
+    });
   }
 };
 
@@ -92,48 +84,40 @@ exports.updateMealPlan = async (req, res) => {
     });
 
     if (result && result.success) {
-      res
-        .status(201)
-        .json({
-          status: 201,
-          success: true,
-          data: result.data,
-          message: result.message,
-        });
+      res.status(201).json({
+        status: 201,
+        success: true,
+        data: result.data,
+        message: result.message,
+      });
     } else {
       res
         .status(400)
         .json({ status: 400, success: false, error: result.message });
     }
-
-    
   } catch (error) {
     // console.error(error);
-    res
-      .status(500)
-      .json({
-        status: 500,
-        success: false,
-        error: "Internal Server Error while updating meal plan",
-      });
+    res.status(500).json({
+      status: 500,
+      success: false,
+      error: "Internal Server Error while updating meal plan",
+    });
   }
 };
 
 exports.deleteMealPlan = async (req, res) => {
   try {
     const { plan_id } = req.body;
-    // console.log("plan id ----->>>>>>",plan_id);
+    console.log("plan id ----->>>>>>",plan_id);
     const result = await mealPlanModel.removeMealPlan({ plan_id });
 
     if (result && result.success) {
-      res
-        .status(201)
-        .json({
-          status: 201,
-          success: true,
-          data: result.data,
-          message: result.message,
-        });
+      res.status(201).json({
+        status: 201,
+        success: true,
+        data: result.data,
+        message: result.message,
+      });
     } else {
       res
         .status(400)
@@ -141,12 +125,10 @@ exports.deleteMealPlan = async (req, res) => {
     }
   } catch (error) {
     // console.error(error);
-    res
-      .status(500)
-      .json({
-        status: 500,
-        success: false,
-        error: "Internal Server Error while deleting meal plan",
-      });
+    res.status(500).json({
+      status: 500,
+      success: false,
+      error: "Internal Server Error while deleting meal plan",
+    });
   }
 };
