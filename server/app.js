d385dd13c43f01e0a52a8aa2a9aedee0bb9eb2a2
Nandan42 <46378413+Nandan42@users.noreply.github.com>
2024-03-12 00:26:14 -0700

    tessting hosting on render with cors origin set to *

--- server/app.js
+++ server/app.js
@@ -1,33 +1,38 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const driverRoutes = require('./routes/Driver/driver_routes.js');
-const providerRoutes = require('./routes/provider_sign_up/provider_sign_up_route.js');
-const customerRoutes = require('./routes/customer/customer_routes.js');
-const meal_plan_Routes = require('./routes/meal_plans/meal_plans_route.js');
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 const corsOptions = {
   origin: '*', // Allow requests from any origin
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
-  optionsSuccessStatus: 204,
 };
 
-app.use(cors(corsOptions)); // Apply cors with the specified options globally
+// Apply CORS with the specified options globally
+app.use(cors(corsOptions));
 
+// Parse JSON requests
 app.use(express.json());
+
+// Routes
+const driverRoutes = require('./routes/Driver/driver_routes.js');
+const providerRoutes = require('./routes/provider_sign_up/provider_sign_up_route.js');
+const customerRoutes = require('./routes/customer/customer_routes.js');
+const mealPlanRoutes = require('./routes/meal_plans/meal_plans_route.js');
 
 // Use the driver routes
 app.use('/api/drivers', driverRoutes);
+
+// Use the provider routes
 app.use('/api/providers', providerRoutes);
 
-// Use the customer routes acting as a middleware
+// Use the customer routes
 app.use('/api/customer', customerRoutes);
 
-// Use the meal_plans routes acting as a middleware
-app.use('/api/provider/meal_plans', meal_plan_Routes);
+// Use the meal_plans routes
+app.use('/api/provider/meal_plans', mealPlanRoutes);
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
