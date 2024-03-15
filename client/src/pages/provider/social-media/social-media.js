9bf78adde748dd44edf13879a65f4b90ab68ad63
Sanjeev <sanjeevmehta500@gmail.com>
2024-03-15 03:59:14 -0700

    feat: Track Deliveries UI and functionality
    This commit contains Track Deliveries UI and functionality Jira Ticket #107.

--- client/src/pages/provider/social-media/social-media.js
+++ client/src/pages/provider/social-media/social-media.js
@@ -5,9 +5,14 @@ import "./social-media.css";
 import Lottie from 'react-lottie';
 import progressAnimation from '../../../component-assets/progressAnimation.json';
 import SideBarMenu from "../../../components/NewSideMenu/NewSideMenu";
+import { useState } from "react";
+import axios from "axios";
 // import Header from "../../../components/header/header";
 
 const SocialMedia=()=> {
+
+  const [selectedFile, setSelectedFile] = useState(null);
+
 
     const defaultOptions = {
       loop: true,
@@ -18,6 +23,38 @@ const SocialMedia=()=> {
       }
     };
 
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    try {
+      // Make a POST request to the Graph API to upload the file
+      const response = await axios.post(
+        `https://graph.facebook.com/2651837188309953/photos`,
+        formData,
+        {
+          params: {
+            access_token: 'EAANNECmoxmABO671C6Q3narVYAulssu0TZBLizYlO3uCYqmoBc4YfjBOqZBq2AmnkJ7fSBq9VkwAZAyixKWaQDZASHW8gT3SJfLrabuBK4gmYjOz44KRpxsMG5z1rzqO20WOBErR3FpkUYYrB0V64eNv5mjeU9vZBAxZACUijCkaAfCatwRc4B6NSZBVr7AaFAwWHTaKhx0AXQWiHRoZAsYY35xudgu1zIyS0Ppxt0i9bdLTevQUIZBwZB',
+            caption: 'Uploaded from my React app!',
+          },
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+
+      console.log('Upload successful:', response.data);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  };
+
+
   return (
     <div className="dashboard-container">
     <div className = "mobileSideMenu">
@@ -26,9 +63,13 @@ const SocialMedia=()=> {
     <div className="sideBarMenu">
         <SideBarMenu currentPage='social-media'/>
       </div> 
-    <div className="WIP">
+    {/* <div className="WIP">
     <h1 className="dashboard-header">Work In Progress</h1>
     <Lottie options={defaultOptions} height={350} width={350} />
+    </div> */}
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
     </div>
     
     </div>
---
+++ client/src/pages/provider/track-deliveries/track-driver/track-driver.js
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from "react";
+import SideBarMenu from "../../../../components/NewSideMenu/NewSideMenu.js";
+import { provider_id } from "../../../../util/localStorage.js";
+import apiHelper from "../../../../util/ApiHelper/ApiHelper.js";
+import Loader from "../../../../components/Loader/Loader.jsx";
+import { ENDPOINTS } from "../../../../apiConfig.js.js";
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+import TrackProviderMap from "../../../../components/Maps/TrackProviderMap/TrackProviderMap.jsx";
+import './track-driver.css'
+import locationMarker2 from "../../../../component-assets/LocationMarker2.svg"
+import phone from "../../../../component-assets/phone.svg"
+import marker from "../../../../component-assets/marker.svg"
+import locationMarker from "../../../../component-assets/locationMarker.svg"
+
+export default function TrackDriver() {
+
+    const [loading, setLoading] = React.useState(false);
+    const [customerData, setCustomerData] = React.useState([]);
+    const [assignTiffinData, setassignTiffinData] = React.useState([]);
+    const location = useLocation();
+
+    // Parse the query string to get the driverID and rowData parameters
+    const { rowData } = queryString.parse(location.search);
+
+    // Decode the rowData parameter and parse it back to an object
+    const row = rowData ? JSON.parse(decodeURIComponent(rowData)) : null;
+
+    useEffect(() => {
+        const assignTiffin = async (id) => {
+            setLoading(true);
+            try {
+                const res = await apiHelper.get(
+                    `${ENDPOINTS.GET_ASSIGNED_TIFFIN}driver_id=${row.driver_id}`
+                );
+                console.log(res);
+                const customers = res.data.map(i => ({
+                    id: i.customers.customer_id,
+                    name: i.customers.name,
+                    address: i.customers.address,
+                    deliveryStatus: 'Pending',
+                    position: { lat: parseFloat(i.customers.latitude), lng: parseFloat(i.customers.longitude) }
+                }));
+                setCustomerData([]);
+                setCustomerData(prevCustomerData => [...prevCustomerData, ...customers]);
+                setassignTiffinData(res.data);
+                setLoading(false);
+            } catch (error) {
+                console.log(error);
+                setLoading(false);
+            }
+        };
+        assignTiffin(`${provider_id}`);
+    }, []);
+
+    const handlePhoneClick = () => {
+        const phoneNumber = row.contact;
+        window.open(`tel:${phoneNumber}`, '_blank');
+      };
+
+
+    return (
+        <div className="track-driver-container">
+            <Loader loading={loading} />
+            <div className="sideBarMenu">
+                <SideBarMenu currentPage="/trackDriver" />
+            </div>
+            <div className="track-map-container">
+            <h2>Tracking {row.name}</h2>
+                {customerData && (
+                    <TrackProviderMap customerData={customerData} driver_id={row.driver_id} />
+                )}
+
+            </div>
+            <div className="delivery-status">
+                {assignTiffinData.length > 0 ? (
+                    <>
+                        <div className="ongoing-delivery">
+                            <h2>Ongoing Delivery</h2>
+                            <div className="delivering-to">
+                                <img src={marker} alt="" className="marker" />
+                                <p>Delivering to: {assignTiffinData[0].customers.name}</p>
+                            </div>
+                            <div className="address">
+                                <img src={locationMarker2} alt="" className="locationMarker2" />
+                                <p>Address: {assignTiffinData[0].customers.address}</p>
+                            </div>
+
+                            <div className="driver-detail">
+                                <div className="driver-detail1">
+                                    <p>Driver: {row.name}</p>
+                                    <p>{row.address}</p>
+                                </div>
+                                <div className="driver-detail2">
+                                    <img src={phone} alt="" className="phone" onClick={handlePhoneClick} />
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className="next-delivery-queue">
+                            <h2>Next Deliveries In Queue</h2>
+                            <ul>
+                                {assignTiffinData.slice(1).map((customer, index) => (
+                                    <li key={index} className="delivery-item">
+                                    <img src={locationMarker} alt="" className="locationMarker" />
+                                    <span className="customer-name">{customer.customers.name}
+                                    </span>
+                                </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </>
+                ) : (
+                    <p>No delivery data available.</p>
+                )}
+            </div>
+        </div>
+
+    );
+
+}
